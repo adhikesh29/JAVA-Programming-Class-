d0d8292348347fdb493f11dfa05ebc9ed4adee9d
adhikesh29 <112157957+adhikesh29@users.noreply.github.com>
2022-12-11 22:02:00 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Test8
+{
+	static Scanner s=new Scanner(System.in);
+	public static void main(String [] args)
+	{
+		System.out.println("Enter Your Id");
+		int id=s.nextInt();//<----10
+		System.out.println("The Entered Id is :"+id);
+	}
+}
